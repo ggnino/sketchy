5fd4cd51da1c21bd3296de182fb7ad61109faac7
Guillermo Gonzalez <ggnino18@gmail.com>
2023-10-14 10:51:26 -0400

    Added Color Change Functionality
    Added the randomRGB() function for randomizing the color Change
    on the grid.

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ function createGrid(gridSize) {
   for (let count = 0; count < gridSize; count++) {
     let gridRowContainer = document.createElement("div");
     gridRowContainer.setAttribute("id", `grid-row-${count + 1}`);
+    gridRowContainer.setAttribute("class", `grid-row`);
 
     for (let counter = 0; counter < gridSize; counter++) {
       let gridSquare = document.createElement("div");
       gridSquare.setAttribute("id", "grid " + (counter + 1));
       gridSquare.setAttribute("class", "grid-square");
+
       gridSquare.addEventListener("mouseenter", () => {
         console.log("Hovered!");
-        gridSquare.style.backgroundColor = "red";
+        gridSquare.style.backgroundColor = `rgb(${randomRGB()})`;
       });
       gridRowContainer.appendChild(gridSquare);
     }
@@ -24,6 +26,18 @@ function createGrid(gridSize) {
   gridContainer.append(...grid);
   return gridContainer;
 }
+// function for randomizing the color
+function randomRGB() {
+  let color = "";
+
+  for (let count = 1; count <= 3; count++) {
+    if (count == 3) color = color + `${Math.floor(Math.random() * 255)}`;
+    else color = color + `${Math.floor(Math.random() * 255)},`;
+  }
+  console.log(color);
+  return color;
+}
+
 // function for displaying sketch grid
 function displayGrid() {
   const body = document.getElementsByTagName("body")[0];
