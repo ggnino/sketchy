71425fb1ecf7af68c504432158a714b1ed2e38a5
Guillermo Gonzalez <ggnino18@gmail.com>
2023-10-14 21:13:58 -0400

    Added GRID SIZE BTN
    Added the GRID SIZE btn with the changeGridSize() function for
    when clicked

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // function for creating the sketch grid
 function createGrid(gridSize) {
   let gridContainer = document.createElement("div");
-  gridContainer.setAttribute("id", "sketchyPad-container");
-
   let grid = [];
+  gridContainer.setAttribute("id", "sketchyPad-container");
 
   for (let count = 0; count < gridSize; count++) {
     let gridRowContainer = document.createElement("div");
@@ -55,17 +54,51 @@ function randomRGB() {
 
   return color;
 }
+// function for changing the grid size when GRID SIZE is clicked
+function changeGridSize() {
+  let userSize = prompt("Change the grid size(max: 100): ");
+  let GRID = document.getElementById("sketchyPad-container");
+
+  while (
+    isNaN(Number(userSize)) ||
+    Number(userSize) < 1 ||
+    Number(userSize) > 100
+  ) {
+    alert("Only numbers (1-100) are accepted. Try again.");
+    userSize = prompt("Change the grid size(max: 100): ");
+  }
+  GRID = createGrid(userSize);
+  GRID.setAttribute("class", "flex");
+
+  const body = document.getElementsByTagName("body")[0];
+
+  body.replaceChild(GRID, body.children[3]);
+  document.getElementById("grid-btn").textContent = `GRID SIZE (${userSize})`;
+}
 
 // function for displaying sketch grid
 function displayGrid() {
   const body = document.getElementsByTagName("body")[0];
   const heading = document.createElement("h1");
+  const btnContainer = document.createElement("div");
+  const gridBtn = document.createElement("button");
+  const rgbBtn = document.createElement("button");
   const SIZE = 16;
   const GRID = createGrid(SIZE);
 
+  gridBtn.addEventListener("click", changeGridSize);
+  gridBtn.textContent = `GRID SIZE (${SIZE})`;
+  rgbBtn.textContent = "RGB";
   heading.textContent = "Sketchy Pad";
 
-  body.append(heading, GRID);
+  body.setAttribute("class", "flex");
+  GRID.setAttribute("class", "flex");
+  gridBtn.setAttribute("class", "grid-btn");
+  gridBtn.setAttribute("id", "grid-btn");
+  rgbBtn.setAttribute("class", "grid-btn");
+
+  btnContainer.append(gridBtn, rgbBtn);
+  body.append(heading, btnContainer, GRID);
 }
 
 // display grtd
